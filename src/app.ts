efc8d8a16a2c529b749df3d21e016801ee73b542
KozaX <55134615+KozaX@users.noreply.github.com>
2020-10-01 13:28:04 +0200

    add intersection types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
 // Code goes here!
+interface Admin {
+  name: string;
+  privileges: string[];
+}
+
+interface Employee {
+  name: string;
+  startDate: Date; //type as well
+}
+
+type ElevatedEmployee = Admin & Employee;
+// interface ElevatedEmployee extends Employee, Admin {}
+
+const e1: ElevatedEmployee = {
+  name: 'Pedro',
+  privileges: ['create-server'],
+  startDate: new Date()
+};
+
+type Combinale = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinale & Numeric;
